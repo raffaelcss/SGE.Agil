ff15f1cf10d7a4371fb637d5fda43f3af8a4120d
Raffael <raffael@pop-os.localdomain>
2022-04-25 10:30:38 -0300

    lista_turmas functions complete

--- lista_turmas.js
+++ lista_turmas.js
@@ -1,55 +1,55 @@
-var equals = function (object1, object2) {
-    // Realiza a verificação das propriedades dos objetos.
-    var prop1 = Object.getOwnPropertyNames(object1);
-    var prop2 = Object.getOwnPropertyNames(object1);
+// var equals = function (object1, object2) {
+//     // Realiza a verificação das propriedades dos objetos.
+//     var prop1 = Object.getOwnPropertyNames(object1);
+//     var prop2 = Object.getOwnPropertyNames(object1);
 
-    // Realiza a verificação se ambos objetos possuem o mesmo número de 
-    // propriedades. Caso contrário, já retorna dizendo que são diferentes.
-    if(prop1.length !== prop2.length)
-        return false;
+//     // Realiza a verificação se ambos objetos possuem o mesmo número de 
+//     // propriedades. Caso contrário, já retorna dizendo que são diferentes.
+//     if(prop1.length !== prop2.length)
+//         return false;
 
-    // Aqui, está sendo verificado se o objeto possui alguma propriedade.
-    // Será usado quando for chamada a função na sua forma recursiva,
-    // para verificar valores literais.
-    if(prop1.length === 0)
-        if(object1 === object2)
-            return true;
-        else
-            return false;
+//     // Aqui, está sendo verificado se o objeto possui alguma propriedade.
+//     // Será usado quando for chamada a função na sua forma recursiva,
+//     // para verificar valores literais.
+//     if(prop1.length === 0)
+//         if(object1 === object2)
+//             return true;
+//         else
+//             return false;
 
-    // Se forem iguais, realiza uma iteração por todas as propriedades.
-    for(var i = 0; i < prop1.length; i++) {
-    // Guarda o valor da propriedade atual na variável "prop".
-        var prop = prop1[i];
+//     // Se forem iguais, realiza uma iteração por todas as propriedades.
+//     for(var i = 0; i < prop1.length; i++) {
+//     // Guarda o valor da propriedade atual na variável "prop".
+//         var prop = prop1[i];
 
-    // Aqui está o pulo do gato.
-    // Verifica se o valor e o tipo das duas propriedades são iguais.
-    // Se sim, somente pula para a próxima iteração. Caso contrário,
-    // podem ser duas coisas: ou são realmente distintos, ou é um objeto,
-    // que, ao comparar as referências, retorna sempre falso.
-    // Para ter certeza da informação, é chamada a mesma função de forma
-    // recursiva, mandando, por parâmetro, os dois objetos que ficou a dúvida.
-    // Se forem iguais, ou se tiverem mais algum objeto internamente, 
-    // a função continuará a ser chamada recursivamente, até chegar ao
-    // ponto de ser um valor literal. Ou, então, retornará falso, pois não
-    // são iguais.
-    // Caso sejam iguais, a função só continuará para a próxima iteração.
-    // Caso contrário, a função já informa que não são dois objetos iguais.
-        if(object1[prop] !== object2[prop]){
-            if(equals(object1[prop], object2[prop]))
-                continue;
-            else
-                return false;
-        }
-    }
-    // Se chegou até aqui e aguentou todas as verificações...
-    // Os objetos são iguais!
-    return true;
-}
+//     // Aqui está o pulo do gato.
+//     // Verifica se o valor e o tipo das duas propriedades são iguais.
+//     // Se sim, somente pula para a próxima iteração. Caso contrário,
+//     // podem ser duas coisas: ou são realmente distintos, ou é um objeto,
+//     // que, ao comparar as referências, retorna sempre falso.
+//     // Para ter certeza da informação, é chamada a mesma função de forma
+//     // recursiva, mandando, por parâmetro, os dois objetos que ficou a dúvida.
+//     // Se forem iguais, ou se tiverem mais algum objeto internamente, 
+//     // a função continuará a ser chamada recursivamente, até chegar ao
+//     // ponto de ser um valor literal. Ou, então, retornará falso, pois não
+//     // são iguais.
+//     // Caso sejam iguais, a função só continuará para a próxima iteração.
+//     // Caso contrário, a função já informa que não são dois objetos iguais.
+//         if(object1[prop] !== object2[prop]){
+//             if(equals(object1[prop], object2[prop]))
+//                 continue;
+//             else
+//                 return false;
+//         }
+//     }
+//     // Se chegou até aqui e aguentou todas as verificações...
+//     // Os objetos são iguais!
+//     return true;
+// }
 
 
 function Turmas_novas(json1, json2){
-    //JSON1 Salvo no Cookie
+    //JSON1 Salvo no LocalStorage
     //JSON2 Atual    
     var obj1 = JSONToobj(json1);
     var obj2 = JSONToobj(json2);
@@ -64,6 +64,33 @@ function Turmas_novas(json1, json2){
         if (!igual){
             novas.push(element);
         }
+    })
+
+    return novas;
+}
+
+function UCs_novas(json1, json2){
+    //JSON1 Salvo no LocalStorage
+    //JSON2 Atual    
+    var obj1 = JSONToobj(json1);
+    var obj2 = JSONToobj(json2);
+    
+    var novas = [];
+    obj2.Turmas.forEach(t_ant => {
+        obj1.Turmas.forEach(t_nova => {
+            if ((t_ant.Nome === t_nova.Nome) && (t_ant.Qtd_ucs !== t_nova.Qtd_ucs)) {
+                t_nova.Ucs.forEach(uc_nova => {
+                    var igual = false;
+                    t_ant.Ucs.forEach(uc_ant => {
+                        if (uc_ant.Nome === uc_nova.Nome)
+                            igual = true;
+                    })
+                    if (!igual){
+                        novas.push(uc_nova);
+                    }
+                })
+            }
+        }) 
     })
 
     return novas;
@@ -120,3 +147,22 @@ function Json_Principal(ul_pai){
 
     return objToJSON(subObj);
 }
+
+let turmas_antigas = localStorage['SGE-Ágil-Turmas_atuais'];
+
+let turmas_atuais = Json_Principal(document.getElementById("ctl24_EduTurmasProfRadioButtonWebForm1_xtabPeriodosLetivos_xpnlTurmaDisciplina"));
+
+let turmas_novas = Turmas_novas(turmas_antigas, turmas_atuais);
+let ucs_novas    = UCs_novas(turmas_antigas, turmas_atuais);
+
+console.log(turmas_novas);
+console.log(ucs_novas);
+
+//mensagens
+if (turmas_novas.length > 0) {
+    alert(`Você possui ${turmas_novas.length} turma${turmas_novas.length > 1 ? "s" : ""} nova${turmas_novas.length > 1 ? "s" : ""}.
+Teste.
+    `)
+}
+
+//localStorage['SGE-Ágil-Turmas_atuais'] = turmas_atuais;
