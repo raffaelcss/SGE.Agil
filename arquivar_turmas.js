c45e3e88146ecff7a84d83cc4d12d2ee52354bbe
Raffael <raffael@pop-os.localdomain>
2022-04-26 16:06:21 -0300

    colapsando todas as turmas arquivadas

--- arquivar_turmas.js
+++ arquivar_turmas.js
@@ -27,8 +27,13 @@ container_arquivadas.children[1].id = "div_arquivadas_hide";
 var div_turmas_arquivadas = container_arquivadas.children[0].children[0].children[0];
 
 for (var i = div_turmas_arquivadas.children.length - 1; i >= 0; i--){
-    if (div_turmas_arquivadas.children[i].classList.contains("archived")){
+    if (!div_turmas_arquivadas.children[i].classList.contains("archived")){
         div_turmas_arquivadas.children[i].remove();
+    } else {
+        //Garantindo que as turmas arquivadas fiquem colapsadas, pois não podem ser abertas enquanto estão arquivadas.
+        div_turmas_arquivadas.children[i].getElementsByClassName("dxnb-header")[0].style.display = 'none';
+        div_turmas_arquivadas.children[i].getElementsByClassName("dxnb-headerCollapsed")[0].style.display = 'block';
+        div_turmas_arquivadas.children[i].getElementsByClassName("dxnb-header")[0].style.display = 'none';
     }
 }
 
