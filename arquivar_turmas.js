12faf6304dfcf69ce395b83844c1720aa7e49f10
Raffael <raffael@pop-os.localdomain>
2022-05-03 11:24:42 -0300

    js style transfer to css

--- arquivar_turmas.js
+++ arquivar_turmas.js
@@ -1,3 +1,7 @@
+//////////////////////////////////////////////////////////////////////////
+////////////////////////////    Constantes    ////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+
 const path_archived = document.createElementNS('http://www.w3.org/2000/svg', "path");
 path_archived.classList.add("path_archived");
 const path_to_archive = document.createElementNS('http://www.w3.org/2000/svg', "path");
@@ -8,16 +12,30 @@ path_archived.setAttribute('d', 'M 23.28 3.055 L 21.444 0.834 C 21.088 0.384 20.
 path_to_archive.setAttribute('d', 'M 21.325 5.187 L 12.065 5.187 L 9.749 2.863 L 2.806 2.863 C 1.531 2.863 0.501 3.909 0.501 5.187 L 0.49 19.132 C 0.49 20.412 1.531 21.458 2.806 21.458 L 21.325 21.458 C 22.598 21.458 23.64 20.412 23.64 19.132 L 23.64 7.512 C 23.64 6.233 22.598 5.187 21.325 5.187 Z M 21.325 19.132 L 2.806 19.132 L 2.806 7.512 L 21.325 7.512 L 21.325 19.132 Z');
 
 
-function insertAfter(newElement, reference) {
-    reference.parentNode.insertBefore(newElement, reference.nextSibling);
-}
-
 var main_container = document.getElementById("ctl24_EduTurmasProfRadioButtonWebForm1_xtabPeriodosLetivos");
 var container_turmas = document.getElementById("ctl24_EduTurmasProfRadioButtonWebForm1_xtabPeriodosLetivos_CC");
 var li_turmas = document.getElementById("ctl24_EduTurmasProfRadioButtonWebForm1_xtabPeriodosLetivos_xpnlTurmaDisciplina");
 
-//Adicionando classe archived às turmas com base no JSON salvo em localstorage
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////    Funções    /////////////////////////////
+//////////////////////////////////////////////////////////////////////////
 
+function insertAfter(newElement, reference) {
+    reference.parentNode.insertBefore(newElement, reference.nextSibling);
+}
+
+function bnt_arquivar() {
+    let inicio_id = this.id.indexOf('_');
+    document.getElementById(this.id.substring(inicio_id+1)).classList.add("archived");
+}
+
+
+
+//////////////////////////////////////////////////////////////////////////
+///////////////////////////////    Script   //////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+
+//Adicionando classe archived às turmas com base no JSON salvo em localstorage
 let turm_local_str = JSONToobj(turmas_antigas);
 console.log(turm_local_str);
 
@@ -34,39 +52,87 @@ for (turm of li_turmas.children){
 
 var container_arquivadas = container_turmas.cloneNode(true);       //true para clonar os filhos
 container_arquivadas.id = "container_arquivadas";
-container_arquivadas.style.display = 'none';
-container_arquivadas.style.position = 'relative';
-container_arquivadas.children[0].id = "div_arquivadas";
-container_arquivadas.children[1].id = "div_arquivadas_hide";
+container_arquivadas.classList.add("container_arquivadas");
+container_arquivadas.classList.toggle("container_hidden");
 
-//Removendo turmas não arquivadas da sessão de arquivadas
-let div_turmas_arquivadas = container_arquivadas.children[0].children[0].children[0];
-
-for (var i = div_turmas_arquivadas.children.length - 1; i >= 0; i--){
-    if (!div_turmas_arquivadas.children[i].classList.contains("archived")){
-        //Turmas não arquivadas
-        div_turmas_arquivadas.children[i].remove();
-    } else {
-        //Turmas arquivadas
-        //Garantindo que as turmas arquivadas fiquem colapsadas, pois não podem ser abertas enquanto estão arquivadas.
-        div_turmas_arquivadas.children[i].getElementsByClassName("dxnb-header")[0].style.display = 'none';
-        div_turmas_arquivadas.children[i].getElementsByClassName("dxnb-headerCollapsed")[0].style.display = 'block';
-        div_turmas_arquivadas.children[i].getElementsByClassName("dxnb-headerCollapsed")[0].style.cursor = 'auto';
-        div_turmas_arquivadas.children[i].getElementsByClassName("dxnb-content")[0].style.display = 'none';
+if (container_arquivadas.children.length > 0) {
+    try {
+        container_arquivadas.children[0].id = "div_arquivadas";
+        container_arquivadas.children[0].classList.add("div_arquivadas");
+        
+        container_arquivadas.children[1].id = "div_arquivadas_hide";
+        container_arquivadas.children[1].classList.add("div_arquivadas_hide");
+    }
+    catch (e) {
+        console.log("Erro 1");
     }
 }
 
+//Removendo turmas não arquivadas da sessão de arquivadas
+let div_turmas_arquivadas;
+try{
+    div_turmas_arquivadas = container_arquivadas.getElementsByClassName("div_arquivadas")[0].children[0].children[0];
+}
+catch (e){
+    console.log("Erro 2");
+}
+
+try {
+    for (var i = div_turmas_arquivadas.children.length - 1; i >= 0; i--){
+        try{
+            if (!div_turmas_arquivadas.children[i].classList.contains("archived")){
+                //Turmas não arquivadas
+                div_turmas_arquivadas.children[i].remove();
+            } else {
+                //Turmas arquivadas
+                //Garantindo que as turmas arquivadas fiquem colapsadas, pois não podem ser abertas enquanto estão arquivadas.
+                div_turmas_arquivadas.children[i].getElementsByClassName("dxnb-header")[0].style.display = 'none';
+                div_turmas_arquivadas.children[i].getElementsByClassName("dxnb-headerCollapsed")[0].style.display = 'block';
+                div_turmas_arquivadas.children[i].getElementsByClassName("dxnb-headerCollapsed")[0].style.cursor = 'auto';
+                div_turmas_arquivadas.children[i].getElementsByClassName("dxnb-content")[0].style.display = 'none';
+            }
+        }
+        catch (e) {
+
+        }
+    }
+}
+catch (e) {
+
+}
+
 //Removendo margin bottom errado que fica na ultima turma das arquivadas
-div_turmas_arquivadas.children[div_turmas_arquivadas.children.length - 1].classList.add("dxnb-last");
+try {
+    div_turmas_arquivadas.children[div_turmas_arquivadas.children.length - 1].classList.add("dxnb-last");
+}
+catch (e) {
+
+}
 
 //Removendo turmas arquivadas da sessão das não arquivadas
-let div_turmas_n_arquivadas = container_turmas.children[0].children[0].children[0];
+let div_turmas_n_arquivadas;
+try {
+    div_turmas_n_arquivadas = container_turmas.children[0].children[0].children[0];
+}
+catch (e) {
 
-for (var i = div_turmas_n_arquivadas.children.length - 1; i >= 0; i--){
-    if (div_turmas_n_arquivadas.children[i].classList.contains("archived")){
-        //Turmas não arquivadas
-        div_turmas_n_arquivadas.children[i].remove();
+}
+
+try {
+    for (var i = div_turmas_n_arquivadas.children.length - 1; i >= 0; i--){
+        try {
+            if (div_turmas_n_arquivadas.children[i].classList.contains("archived")){
+                //Turmas não arquivadas
+                div_turmas_n_arquivadas.children[i].remove();
+            }
+        }
+        catch (e) {
+
+        }
     }
+}
+catch (e) {
+
 }
 
 //Adicionar div de listras nas turmas arquivadas
@@ -74,31 +140,19 @@ const div_grid = document.createElement("div");
 div_grid.classList.add("grid");
 div_turmas_arquivadas.appendChild(div_grid);
 
-//Depois transferir todo estilo para o CSS basic
 
-//Adicionando botão Turmas Arquivadas
+//Adicionando Switch para exibir Turmas Arquivadas
 const div_head_turmas = document.createElement("div");
 div_head_turmas.id = "div_head_turmas";
-div_head_turmas.style.display = 'flex';
-div_head_turmas.style.alignItems = 'center';
-div_head_turmas.style.padding = '0px 5px 5px';
-div_head_turmas.style.height = '29px';
+div_head_turmas.classList.add("div_head_turmas");
 
 const texto_swt_arq = document.createElement("span");
 texto_swt_arq.innerHTML = "Turmas arquivadas";
 texto_swt_arq.id = "texto_swt_arq";
-texto_swt_arq.style.paddingRight = '10px';
-texto_swt_arq.style.fontSize = '12px';
-texto_swt_arq.style.fontWeight = '600';
+texto_swt_arq.classList.add("texto_swt_arq");
 
 const div_bnt_arq = document.createElement("div");
-div_bnt_arq.style.marginLeft = 'auto';
-div_bnt_arq.style.display = 'flex';
-div_bnt_arq.style.alignItems = 'center';
-div_bnt_arq.style.border = 'solid 1px #8b8b8b';
-div_bnt_arq.style.marginRight = '8px';
-div_bnt_arq.style.padding = '5px';
-div_bnt_arq.style.borderRadius = '5px';
+div_bnt_arq.classList.add("div_bnt_arq");
 
 const label_bnt_arq = document.createElement("label");
 label_bnt_arq.classList.add("switch");
@@ -112,6 +166,7 @@ span_bnt_arq.classList.add("slider");
 span_bnt_arq.classList.add("round");
 span_bnt_arq.id = "swt_archived";
 
+//Afiliando cada componente
 div_head_turmas.appendChild(document.getElementById("ctl24_EduTurmasProfRadioButtonWebForm1_RMWLabel1"));
 div_head_turmas.appendChild(div_bnt_arq);
     div_bnt_arq.appendChild(texto_swt_arq);
@@ -120,9 +175,15 @@ div_head_turmas.appendChild(div_bnt_arq);
         label_bnt_arq.appendChild(span_bnt_arq);
 
 
-insertAfter(div_head_turmas, document.getElementById("MainContainer").children[1]);
+//Inserindo no menu princpal
+try {
+    insertAfter(div_head_turmas, document.getElementById("MainContainer").children[1]);
+}
+catch (e) {
 
-//Removendo espaços extras
+}
+
+//Removendo espaços extras (<BR>)
 Array.from(document.getElementById("MainContainer").getElementsByTagName("br")).forEach((element) => {
     element.remove();
 });
@@ -136,24 +197,35 @@ chk_bnt_arq.addEventListener('click', () => {
     }
 })
 
-main_container.appendChild(container_arquivadas);
+try {
+    main_container.appendChild(container_arquivadas);
+}
+catch (e) {
 
-console.log("adicionadas turmas arquivadas");
+}
 
-
+//Função do Switch de exibir turmas arquivadas
 chk_bnt_arq.addEventListener('change', () => {
-    container_turmas.style.display = chk_bnt_arq.checked ? 'none' : 'block';
-    container_arquivadas.style.display = chk_bnt_arq.checked ? 'block' : 'none';
+    container_turmas.classList.toggle("container_hidden");
+    container_arquivadas.classList.toggle("container_hidden");
 });
 
 
-//Adicionando botão de arquivar em cada turma
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////
+/////////////    Adicionando botão de arquivar em cada turma   ///////////
+//////////////////////////////////////////////////////////////////////////
 
 const div_bnt_archive = document.createElement("div");
-div_bnt_archive.style.display = 'block';
-div_bnt_archive.style.position = 'absolute';
-div_bnt_archive.style.left = 'calc(100% - 20px - 7px)';
-div_bnt_archive.style.top = '4px';
+div_bnt_archive.classList.add("div_bnt_archive");
+
 
 const svg_to_archive = document.createElementNS('http://www.w3.org/2000/svg', 'svg'); //Necessário para diferenciar viewbox de viewBox
 svg_to_archive.classList.add("svg_bnt");
@@ -165,19 +237,17 @@ svg_to_archive.setAttribute('height', '20');
 svg_to_archive.setAttribute("viewBox", '0 0 24 24');
 
 div_bnt_archive.appendChild(svg_to_archive);
-
 svg_to_archive.appendChild(path_to_archive);
 
 Array.from(li_turmas.children).forEach(li => {
     let div_temp = div_bnt_archive.cloneNode(true);
-    div_temp.getElementsByClassName('svg_bnt')[0].id = 'bntToArchive_' + li.id;
-    li.appendChild(div_temp);
-    div_temp.getElementsByClassName('svg_bnt')[0].addEventListener('click', bnt_arquivar);
+    try {
+        div_temp.getElementsByClassName('svg_bnt')[0].id = 'bntToArchive_' + li.id;
+        li.appendChild(div_temp);
+        div_temp.getElementsByClassName('svg_bnt')[0].addEventListener('click', bnt_arquivar);
+    }
+    catch (e) {
+
+    }
 });
-
-
-function bnt_arquivar() {
-    let inicio_id = this.id.indexOf('_');
-    document.getElementById(this.id.substring(inicio_id+1)).classList.add("archived");
-}
 
