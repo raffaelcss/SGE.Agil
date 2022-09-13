2eb5949a47d161660cf1e35d1da326ccfceacbd5
raffaelcss <raffaelcss@hotmail.com>
2022-09-12 21:17:21 -0300

    change onClicked function popup

--- background.js
+++ background.js
@@ -1,19 +1,76 @@
-//Impede que o menu da extenção apareceça em outros sites quando você clicar
+//Executado quando instala a extensão
 
-//Descomentar para ativar função ↴
-chrome.action.disable();
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+// chrome.runtime.onInstalled.addListener(() => {
+//   // default state goes here
+//   // this runs ONE TIME ONLY (unless the user reinstalls your extension)
+//   alert("Teste instalação");
+// });
+
+//Verifica aba por aba e define se irá ativar o popUp ou clique
+chrome.tabs.onUpdated.addListener((tabIdObt, changeInfo, tab) => {
   if (changeInfo.status == 'complete'){
-    console.log(tab.url);
+    let caminhoPopup = "";
     //Define qual a palavra deve conter a URL para que ative a extenção. Ex: fiemg
     if (tab.url.indexOf('fiemg.com.br') != -1 && tab.url.indexOf('Corpore') != -1){
       console.log('SGE.Ágil Enable!');
-      chrome.action.enable(tabId);
+      caminhoPopup = "index.html";
     }
     else{
       console.log('SGE.Ágil Disable!');
-      chrome.action.disable(tabId);
+      caminhoPopup = "";
     }
+    //Ativa ou desativa o popUp
+    chrome.action.setPopup({popup:caminhoPopup, tabId: tabIdObt},()=>{});
   }
 });
-/////////////////////////////////////////////////////////////////////////////
+
+//Ir para o SGE quando clicar
+chrome.action.onClicked.addListener(() => {
+  console.log(tabSGE);
+  if (tabSGE <= 0){
+    chrome.tabs.create({url:'https://www2.fiemg.com.br/Corpore.Net/Login.aspx'}, ()=>{});
+  } else {
+    chrome.tabs.highlight({tabs: tabSGE}, () => {});
+  }
+  let count = 1000;
+  function esperaPagEAbrePopup() { 
+    setTimeout(()=>{
+      chrome.tabs.query({highlighted: true}, (tab) => {
+        if (tab.length > 0){
+          if (tab[0].status == 'complete' || count <= 0){
+            chrome.action.openPopup();
+          } else {
+            console.log("tentando.. " + (1000 - count)*2 + "ms decorridos");
+            count--;
+            esperaPagEAbrePopup();
+          }
+        }
+      })
+    },2);
+  }
+  esperaPagEAbrePopup();
+});
+
+var tabSGE  = 0;
+function getSGEIndex(){
+  tabSGE = 0;
+  chrome.tabs.query({url:'*://www2.fiemg.com.br/Corpore.Net/*'} ,(tab) => {
+    if(tab.length > 0){
+      tabSGE = tab[0].index;
+    }
+  });
+  if (tabSGE == 0){
+    chrome.tabs.query({url:'*://prados101.fiemg.com.br/Corpore.Net/*'} ,(tab) => {
+      if(tab.length > 0)
+        tabSGE = tab[0].index;
+    }); 
+  }
+}
+
+//Atualiza valor do Index da pág do SGE
+chrome.tabs.onCreated.addListener(getSGEIndex);
+chrome.tabs.onDetached.addListener(getSGEIndex);
+chrome.tabs.onMoved.addListener(getSGEIndex);
+chrome.tabs.onRemoved.addListener(getSGEIndex);
+getSGEIndex();
+
