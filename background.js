d57303ae7d7c750819248fd546798c3eadf2b591
raffaelcss <raffaelcss@hotmail.com>
2022-10-03 16:17:18 -0300

    fix background.js last merge

--- background.js
+++ background.js
@@ -7,70 +7,70 @@
 // });
 
 //Verifica aba por aba e define se irá ativar o popUp ou clique
-chrome.tabs.onUpdated.addListener((tabIdObt, changeInfo, tab) => {
-  if (changeInfo.status == 'complete'){
-    let caminhoPopup = "";
-    //Define qual a palavra deve conter a URL para que ative a extenção. Ex: fiemg
-    if (tab.url.indexOf('fiemg.com.br') != -1 && tab.url.indexOf('Corpore') != -1){
-      console.log('SGE.Ágil Enable!');
-      caminhoPopup = "index.html";
-    }
-    else{
-      console.log('SGE.Ágil Disable!');
-      caminhoPopup = "";
-    }
-    //Ativa ou desativa o popUp
-    chrome.action.setPopup({popup:caminhoPopup, tabId: tabIdObt},()=>{});
-  }
-});
+// chrome.tabs.onUpdated.addListener((tabIdObt, changeInfo, tab) => {
+//   if (changeInfo.status == 'complete'){
+//     let caminhoPopup = "";
+//     //Define qual a palavra deve conter a URL para que ative a extenção. Ex: fiemg
+//     if (tab.url.indexOf('fiemg.com.br') != -1 && tab.url.indexOf('Corpore') != -1){
+//       console.log('SGE.Ágil Enable!');
+//       caminhoPopup = "index.html";
+//     }
+//     else{
+//       console.log('SGE.Ágil Disable!');
+//       caminhoPopup = "";
+//     }
+//     //Ativa ou desativa o popUp
+//     chrome.action.setPopup({popup:caminhoPopup, tabId: tabIdObt},()=>{});
+//   }
+// });
 
 //Ir para o SGE quando clicar
 chrome.action.onClicked.addListener(() => {
-  console.log("Window: " + windowSGE);
-  console.log("Tab Id: " + tabSGE);
+  console.log(tabSGE);
   if (tabSGE <= 0){
     chrome.tabs.create({url:'https://www2.fiemg.com.br/Corpore.Net/Login.aspx'}, ()=>{});
   } else {
-    chrome.tabs.highlight({tabs: tabSGE, windowId: windowSGE}, () => {});
+    chrome.tabs.highlight({tabs: tabSGE}, () => {});
   }
   let count = 1000;
   function esperaPagEAbrePopup() { 
     setTimeout(()=>{
-      chrome.tabs.query({highlighted: true, currentWindow: true}, (tab) => {
+      chrome.tabs.query({highlighted: true}, (tab) => {
         if (tab.length > 0){
           if (tab[0].status == 'complete' || count <= 0){
             if (tab[0].url.indexOf('fiemg.com.br') != -1 && tab[0].url.indexOf('Corpore') != -1){
-              chrome.action.openPopup({windowId: tab[0].windowId}, () => {});
+              console.log(tab[0]);
+              try {
+                chrome.action.setPopup({popup:"index.html", tabId: tab[0].id},()=>{});
+                chrome.action.openPopup();
+              } catch (error) {
+                console.log("Erro ao abrir popup. code: " + error);
+              }
             }
           } else {
-            console.log("tentando.. " + (1000 - count)*2 + "ms decorridos");
+            console.log("tentando.. " + (1000 - count)*20 + "ms decorridos");
             count--;
             esperaPagEAbrePopup();
           }
         }
       })
-    },2);
+    },20);
   }
   esperaPagEAbrePopup();
 });
 
 var tabSGE  = 0;
-var windowSGE = 0;
 function getSGEIndex(){
   tabSGE = 0;
-  windowSGE = 0;
   chrome.tabs.query({url:'*://www2.fiemg.com.br/Corpore.Net/*'} ,(tab) => {
     if(tab.length > 0){
       tabSGE = tab[0].index;
-      windowSGE = tab[0].windowId;
     }
   });
   if (tabSGE == 0){
     chrome.tabs.query({url:'*://prados101.fiemg.com.br/Corpore.Net/*'} ,(tab) => {
-      if(tab.length > 0){
+      if(tab.length > 0)
         tabSGE = tab[0].index;
-        windowSGE = tab[0].windowId;
-      }
     }); 
   }
 }
@@ -81,3 +81,4 @@ chrome.tabs.onDetached.addListener(getSGEIndex);
 chrome.tabs.onMoved.addListener(getSGEIndex);
 chrome.tabs.onRemoved.addListener(getSGEIndex);
 getSGEIndex();
+
