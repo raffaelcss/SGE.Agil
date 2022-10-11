b3f1f266ca846d69496f0de29e335e893a7d72dd
raffaelcss <raffaelcss@hotmail.com>
2022-10-11 15:28:20 -0300

    styled workbook finalized

--- planilhas_notas.js
+++ planilhas_notas.js
@@ -57,6 +57,18 @@ function corrigirTamanhoColunas(ws, rowCount, columnCount){
         const cellRef = XLSX.utils.encode_cell({ r: 0, c: col });
         ws["!cols"].push({ wch: 12.5 });
     }
+    //Legenda
+    max_width =0;
+    for (let row = 2; row < 7; row++){
+        const cellRef = XLSX.utils.encode_cell({ r: row, c: columnCount+3 });
+        console.log(cellRef);
+        if (ws[cellRef]){
+            max_width = Math.max(ws[cellRef].v.length,max_width);
+        }
+    }
+    ws["!cols"].push({ wch: 3 });
+    ws["!cols"].push({ wch: 9.5 });
+    ws["!cols"].push({ wch: max_width });
 }
 
 function addFormulaSomatorioNotas(ws, rowCount, columnCount){
@@ -71,7 +83,110 @@ function addFormulaSomatorioNotas(ws, rowCount, columnCount){
     }
 }
 
+function adicionaLegenda(ws, legTable, columnCount){
+    const cellRef = XLSX.utils.encode_cell({ r: 1, c: columnCount+2 });
+    XLSX.utils.sheet_add_dom(ws, legTable, {origin: cellRef});
+}
+
+function estilizarLegenda(ws, rowCount, columnCount, qtdAtiv){
+    for (let row = 1; row <= qtdAtiv+1; row++) {
+        for (let col = columnCount+2; col <= columnCount + 3; col++) {
+            const cellRef = XLSX.utils.encode_cell({ r: row, c: col });
+            // Geral
+            ws[cellRef].s = {
+                font: { 
+                    name: "Arial", 
+                    sz: 12 
+                },
+                alignment: { 
+                    horizontal: "center",
+                    // wrapText: true 
+                },
+                border: {
+                    top:{
+                        style: 'thin',
+                    },
+                    bottom:{
+                        style: 'thin',
+                    },
+                    left:{
+                        style: 'thin',
+                    },
+                    right:{
+                        style: 'thin',
+                    },
+                }
+            };
+            //Cabeçalho
+            if (row === 1) {
+                ws[cellRef].s = {
+                    ...ws[cellRef].s,
+                    font: { 
+                        ...ws[cellRef].s.font,
+                        bold: true,
+                        color: { 
+                            rgb: "FFFFFF" 
+                        }
+                    },
+                    fill: {
+                        fgColor: {
+                            rgb: "1F497D"
+                        }
+                    }
+                };
+            }
+
+            //Box Drawin
+            if (row === 1) {
+                ws[cellRef].s = {
+                    ...ws[cellRef].s,
+                    border: {
+                        ...ws[cellRef].s.border,
+                        top:{
+                            style: 'medium',
+                        }
+                    }
+                };
+            }
+            if (row === qtdAtiv+1){
+                ws[cellRef].s = {
+                    ...ws[cellRef].s,
+                    border: {
+                        ...ws[cellRef].s.border,
+                        bottom:{
+                            style: 'medium',
+                        }
+                    }
+                };
+            }
+            if (col === columnCount+2) {
+                ws[cellRef].s = {
+                    ...ws[cellRef].s,
+                    border: {
+                        ...ws[cellRef].s.border,
+                        left:{
+                            style: 'medium',
+                        }
+                    }
+                };
+            }
+            if (col === columnCount + 3){
+                ws[cellRef].s = {
+                    ...ws[cellRef].s,
+                    border: {
+                        ...ws[cellRef].s.border,
+                        right:{
+                            style: 'medium',
+                        }
+                    }
+                };
+            }
+        }
+    }
+}
+
 function estilizarTabela(ws, rowCount, columnCount){
+    //Notas
     for (let row = 1; row <= rowCount; row++) {
         for (let col = 1; col <= columnCount; col++) {
             const cellRef = XLSX.utils.encode_cell({ r: row, c: col });
@@ -114,6 +229,18 @@ function estilizarTabela(ws, rowCount, columnCount){
                     fill: {
                         fgColor: {
                             rgb: "1F497D"
+                        }
+                    }
+                };
+            }
+
+            //Notas
+            if (col >= 6 && col <= columnCount && row >=2 && row <= rowCount ){
+                ws[cellRef].s = {
+                    ...ws[cellRef].s,
+                    fill: {
+                        fgColor: {
+                            rgb: "cccccc"
                         }
                     }
                 };
@@ -165,7 +292,7 @@ function estilizarTabela(ws, rowCount, columnCount){
                 };
             }
         }
-      }
+    }
 }
 
 function criaTabela(type, fn, dl) {
@@ -176,20 +303,6 @@ function criaTabela(type, fn, dl) {
         let texto = td.getElementsByTagName("td")[0].innerText;
         td.innerText = texto;
     });
-
-    // //Adicionando coluna inicial
-    // Array.from(elt.children[0].children).forEach(tr => {
-    //     const colNova = document.createElement("td");
-    //     tr.insertAdjacentElement('afterbegin', colNova);
-    // });
-
-    // //Adicionando linha inicial
-    // const rowNova = document.createElement("tr");
-    // for(let i=0; i < elt.children[0].children[1].children.length; i++){
-    //     let col = document.createElement("td");
-    //     rowNova.appendChild(col);
-    // }
-    // elt.children[0].insertAdjacentElement('afterbegin', rowNova);
     
     //Cria Workbook
 	var wb = XLSX.utils.table_to_book(elt, {sheet:"Notas", origin: "B2"});
@@ -197,14 +310,25 @@ function criaTabela(type, fn, dl) {
     const range = XLSX.utils.decode_range(wb.Sheets['Notas']["!ref"] ?? "");
     const rowCount = range.e.r;
     const columnCount = range.e.c;
-    
-    console.log(elt);
-    console.log(wb);
 
     addFormulaSomatorioNotas(wb.Sheets['Notas'], rowCount, columnCount);
     preencherNotas(wb.Sheets['Notas'], rowCount, columnCount);
+
+    //Corrigindo tabela legenda
+    var legTable = document.getElementById("ctl24_gvLegendaProvas").cloneNode(true);
+    let qtdAtiv = legTable.children[0].children.length-1;
+    Array.from(legTable.children[0].children).forEach(tr => {
+        Array.from(tr.children).forEach(td => {
+            if (td.classList.contains("Invisible")) {
+                td.parentNode.removeChild(td);
+            }
+        });
+    });
+    adicionaLegenda(wb.Sheets['Notas'], legTable, columnCount);
+
     corrigirTamanhoColunas(wb.Sheets['Notas'], rowCount, columnCount);
     estilizarTabela(wb.Sheets['Notas'], rowCount, columnCount);
+    estilizarLegenda(wb.Sheets['Notas'], rowCount, columnCount,qtdAtiv);
 
     console.log(wb);
 
