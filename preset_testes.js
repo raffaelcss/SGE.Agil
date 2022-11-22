a5f886e69c7be923ebb0adfda6226ceb6af2faa1
raffaelcss <raffaelcss@hotmail.com>
2022-11-22 17:00:01 -0300

    modal testes

--- preset_testes.js
+++ preset_testes.js
@@ -23,11 +23,79 @@ function criarModal(){
     const dialog = document.createElement("dialog");
     dialog.id = "dialogPreset";
 
-    const innerHTML = "<h1>Criar Preset de provas</h1>/n<p>Testando</p>";
+    const qtdAtividades = 5;
+
+    let nomesEtapas = [];
+    let etapaAtual = 0;
+    let nomeEtapaAtual = "ctl24_xgvAvaliacao_DXPEForm_efnew_xcbEtapa_DDD_L_LBI" + etapaAtual + "T2";
+    console.log(nomeEtapaAtual);
+    console.log(document.getElementById(nomeEtapaAtual));
+    while (document.getElementById(nomeEtapaAtual)){
+        console.log(nomeEtapaAtual);
+        nomesEtapas.push(document.getElementById(nomeEtapaAtual).innerText);
+        etapaAtual++;
+        nomeEtapaAtual = "ctl24_xgvAvaliacao_DXPEForm_efnew_xcbEtapa_DDD_L_LBI" + etapaAtual + "T2";
+    }
+
+    console.log(nomesEtapas);
+
+    let atividadeHTML = "";
+
+    for (let i=0; i < qtdAtividades; i++){
+        atividadeHTML += `
+            <tr>
+                <td>Avaliação ${i+1}:</td>
+                <td>
+                    <select name="select">
+                        <option value="valor1">Valor 1</option>
+                        <option value="valor2" selected>Valor 2</option>
+                        <option value="valor3">Valor 3</option>
+                    </select>
+                </td>
+                <td>
+                    <input type="text" class="edtAtividade">
+                </td>
+                <td>
+                    <input type="text" class="edtValor">
+                </td>
+            </tr>
+        `;
+    }
+    
+
+    const innerHTML = `
+    <h1>Criar modelo de avaliações</h1>
+    <div>
+        <h2>Nome do modelo:</h2>
+        <input type="text">
+    </div>
+    <table id="teste" style="">
+        <thead>
+            <tr>
+                <th>Avaliação</th>
+                <th>Etapa</th>
+                <th>Nome</th>
+                <th>Valor</th>
+            </tr>
+        </thead>
+        <tbody style="">           
+            ` + atividadeHTML + `
+        </tbody>
+    </table>
+    <div>
+        <button>Salvar</button>
+        <button>Cancelar</button>
+    </div>
+    `;
 
     dialog.innerHTML = innerHTML;
 
     pai.appendChild(dialog);
 }
 
-criarModal();
+let bt = document.createElement("button");
+bt.id = "teste";
+bt.addEventListener("click", ()=>{
+    criarModal();
+});
+document.getElementsByTagName("body")[0].appendChild(bt);
