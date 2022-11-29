f4b59b8707f44a7e130750d09db268943e0c2494
raffaelcss <raffaelcss@hotmail.com>
2022-11-29 18:05:14 -0300

    etapa working

--- preset_testes.js
+++ preset_testes.js
@@ -20,125 +20,177 @@ function Setar_Testes(){
 function criarModal(){
     const pai = document.getElementsByTagName("body")[0];
 
+    const btnIncluir = document.getElementById("ctl24_eduToolBarXGrid_xbtIncluir");
+
     const dialog = document.createElement("dialog");
     dialog.id = "dialogPreset";
 
     let nomesEtapas = [];
     let etapaAtual = 0;
-    let nomeEtapaAtual = "ctl24_xgvAvaliacao_DXPEForm_efnew_xcbEtapa_DDD_L_LBI" + etapaAtual + "T2";
-    console.log(nomeEtapaAtual);
-    console.log(document.getElementById(nomeEtapaAtual));
-    while (document.getElementById(nomeEtapaAtual)){
-        console.log(nomeEtapaAtual);
-        nomesEtapas.push(document.getElementById(nomeEtapaAtual).innerText);
-        etapaAtual++;
-        nomeEtapaAtual = "ctl24_xgvAvaliacao_DXPEForm_efnew_xcbEtapa_DDD_L_LBI" + etapaAtual + "T2";
-    }
-
-    console.log(nomesEtapas);
-
-    var ativAtual = 0;
-
-    function addAtividade(){
-        let tbody = document.getElementById("tbody-ativ");
-        if (!tbody){
-            return;
-        }
-        let trs = tbody.getElementsByClassName("tr-atividade");
-        let atividadeHTML ="";
-
-        Array.from(trs).forEach(tr => {
-            atividadeHTML += `
-                <tr class="tr-atividade">
-                    ${tr.innerHTML}
-                </tr>
-            `;
-        });
-
-        atividadeHTML += `
-            <tr class="tr-atividade">
-                <td>Avaliação ${ativAtual+1}:</td>
-                <td>
-                    <select name="select">
-                        <option value=0 selected>Valor 1</option>
-                        <option value=1>Valor 2</option>
-                        <option value=2>Valor 3</option>
-                    </select>
-                </td>
-                <td>
-                    <input type="text" class="edtAtividade">
-                </td>
-                <td>
-                    <input type="text" class="edtValor">
-                </td>
-            </tr>
-        `;
-        ativAtual++;
-
-        if (ativAtual < 5){
-            //Botão
-            atividadeHTML += `
-                <tr class="tr-botao">
-                    <td colspan="4">
-                        <button id="btn-add-atividade" style="margin: auto;border: 1px solid #747474;color: #747474;border-radius: 10px;text-rendering: geometricPrecision;">+</button>
-                    </td>
-                </tr>
-            `;
-        }
-        
-        //Add
-        tbody.innerHTML = atividadeHTML;
-
-        if (ativAtual < 5){
-            let btn_atv = document.getElementById("btn-add-atividade");
-            if (btn_atv){
-                btn_atv.addEventListener("click", ()=>{
-                    addAtividade();
-                });
-            }
-        }
-        
-    }
     
-    const innerHTML = `
-        <h1 style="text-align: center;">Criar modelo de avaliações</h1>
-        <div style="display: flex;justify-content: space-evenly;margin: 0px 150px;">
-            <h2 style="margin: 0px;">Nome do modelo:</h2>
-            <input type="text" style="width: 180px;">
-        </div>
-        <table id="teste" style="">
-            <thead>
-                <tr>
-                    <th>Avaliação</th>
-                    <th>Etapa</th>
-                    <th>Nome</th>
-                    <th>Valor</th>
-                </tr>
-            </thead>
-            <tbody id="tbody-ativ" style="">           
-                
-            </tbody>
-        </table>
-        <div style="display: flex;justify-content: space-evenly;">
-            <button id="salvar-modelo">Salvar</button>
-            <button id="cancelar-modelo">Cancelar</button>
-        </div>
-    `;
+    //Clica em incluir
+    btnIncluir.click();
+    let tentativas = 1000;
+    function aguardaFormAvaliacao(){
+        setTimeout(()=> {
+            let objAbrirEtapas = document.getElementById("ctl24_xgvAvaliacao_DXPEForm_efnew_xcbEtapa_I");
+            let nomeEtapaAtual = "ctl24_xgvAvaliacao_DXPEForm_efnew_xcbEtapa_DDD_L_LBI" + etapaAtual + "T2";
+            let objEtapaAtual = document.getElementById(nomeEtapaAtual);
+            if (objAbrirEtapas || tentativas <= 0){
+                setTimeout(()=>{
+                    //Exibe etapas
+                    objAbrirEtapas.dispatchEvent(new MouseEvent('mousedown'));
 
-    dialog.innerHTML = innerHTML;
+                    console.log(nomeEtapaAtual);
+                    console.log(document.getElementById(nomeEtapaAtual));
+                    while (document.getElementById(nomeEtapaAtual)){
+                        console.log(nomeEtapaAtual);
+                        nomesEtapas.push(document.getElementById(nomeEtapaAtual).innerText);
+                        etapaAtual++;
+                        nomeEtapaAtual = "ctl24_xgvAvaliacao_DXPEForm_efnew_xcbEtapa_DDD_L_LBI" + etapaAtual + "T2";
+                    }
 
-    pai.appendChild(dialog);
+                    console.log(nomesEtapas);
+                    
+                    var ativAtual = 0;
+                    function addAtividade(){
+                        let tbody = document.getElementById("tbody-ativ");
+                        if (!tbody){
+                            return;
+                        }
+                        let trs = tbody.getElementsByClassName("tr-atividade");
+                        let atividadeHTML ="";
+                        let selecteds = [];
 
-    //Botões
-    let cancelBtn = document.getElementById("cancelar-modelo");
-    if (cancelBtn){
-        cancelBtn.addEventListener("click", ()=>{
-            document.getElementById("dialogPreset").close();
-        })
+                        Array.from(trs).forEach(tr => {
+                            atividadeHTML += `
+                                <tr class="tr-atividade">
+                                    ${tr.innerHTML}
+                                </tr>
+                            `;
+                            //Selecionando correta
+                            selecteds.push(tr.children[1].children[0].value);
+                        });
+
+                        function optionsEtapas(etapas, val){
+                            if (!etapas){
+                                return "";
+                            }
+                            if (etapas.length <= 0){
+                                return "";
+                            }
+                            let html = "";
+                            for(let i=0; i < etapas.length; i++){
+                                html += "<option value="+i+" " + (i==val ? "selected" : "") + ">" + etapas[i] + "</option>\n";
+                            };
+                            return html;
+                        }
+
+                        atividadeHTML += `
+                            <tr class="tr-atividade">
+                                <td>Avaliação ${ativAtual+1}:</td>
+                                <td>
+                                    <select name="select">
+                                        ${optionsEtapas(nomesEtapas)}
+                                    </select>
+                                </td>
+                                <td>
+                                    <input type="text" class="edtAtividade">
+                                </td>
+                                <td>
+                                    <input type="text" class="edtValor" value="0,00">
+                                </td>
+                            </tr>
+                        `;
+                        ativAtual++;
+
+                        if (ativAtual < 5){
+                            //Botão
+                            atividadeHTML += `
+                                <tr class="tr-botao">
+                                    <td colspan="4">
+                                        <button id="btn-add-atividade" style="margin: auto;border: 1px solid #747474;color: #747474;border-radius: 10px;text-rendering: geometricPrecision;">+</button>
+                                    </td>
+                                </tr>
+                            `;
+                        }
+                        
+                        //Add
+                        tbody.innerHTML = atividadeHTML;
+
+                        //Seleciona a etapa correta das atv anteriores ao add nova atv
+                        trs = tbody.getElementsByClassName("tr-atividade");
+                        for(let i=0; i < selecteds.length; i++){
+                            trs[i].children[1].children[0].value = selecteds[i];
+                        }
+
+                        if (ativAtual < 5){
+                            let btn_atv = document.getElementById("btn-add-atividade");
+                            if (btn_atv){
+                                btn_atv.addEventListener("click", ()=>{
+                                    addAtividade();
+                                });
+                            }
+                        }
+                        
+                    }
+                    
+                    const innerHTML = `
+                        <h1 style="text-align: center;">Criar modelo de avaliações</h1>
+                        <div style="display: flex;justify-content: space-evenly;margin: 0px 150px;">
+                            <h2 style="margin: 0px;">Nome do modelo:</h2>
+                            <input type="text" style="width: 180px;">
+                        </div>
+                        <table id="teste" style="">
+                            <thead>
+                                <tr>
+                                    <th>Avaliação</th>
+                                    <th>Etapa</th>
+                                    <th>Nome</th>
+                                    <th>Valor</th>
+                                </tr>
+                            </thead>
+                            <tbody id="tbody-ativ" style="">           
+                                
+                            </tbody>
+                        </table>
+                        <div style="display: flex;justify-content: space-evenly;">
+                            <button id="salvar-modelo">Salvar</button>
+                            <button id="cancelar-modelo">Cancelar</button>
+                        </div>
+                    `;
+
+                    dialog.innerHTML = innerHTML;
+
+                    pai.appendChild(dialog);
+
+                    //Botões
+                    let cancelBtn = document.getElementById("cancelar-modelo");
+                    if (cancelBtn){
+                        cancelBtn.addEventListener("click", ()=>{
+                            document.getElementById("dialogPreset").close();
+                        })
+                    }
+
+                    //Add atividade 1
+                    addAtividade();
+
+                    setTimeout(()=>{
+                        //Fecha menu
+                        document.getElementById("ctl24_xgvAvaliacao_DXPEForm_efnew_DXCBtn1").click();
+                    },500);
+
+                    //Abre modal
+                    document.getElementById("dialogPreset").showModal();
+                },350);
+            } else {
+                tentativas--;
+                aguardaFormAvaliacao();
+                console.log("tentativa: " + (1000-tentativas) + "/1000");
+            }
+        },10);
     }
-
-    //Add atividade 1
-    addAtividade();
+    aguardaFormAvaliacao();
 }
 
 let bt = document.createElement("button");
